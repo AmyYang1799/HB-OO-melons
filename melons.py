a989e2f621d2cb6e1f085abcec3c7b3c14ce9b9b
vagrant <vagrant@vagrant.vm>
2020-10-09 22:03:48 +0000

    added clas GovernmentMelonOrder and completed all lab prompts

--- melons.py
+++ melons.py
@@ -9,7 +9,14 @@ class AbstractMelonOrder():
         """Calculate price, including tax."""
 
         base_price = 5
+        if self.species == "Christmas melon":
+            base_price = base_price * 1.5
+
         total = (1 + self.tax) * self.qty * base_price
+
+        if self.order_type == "international" and self.qty < 10:
+            total += 3
+
 
         return total
 
@@ -33,8 +40,8 @@ class DomesticMelonOrder(AbstractMelonOrder):
 class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
-    order_type = "domestic"
-    tax = .08
+    order_type = "international"
+    tax = .17
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
@@ -48,3 +55,31 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    
+    order_type = "government"
+    tax = 0 
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes."""
+
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.passed_inspection = False
+
+    def __repr__(self):
+        return f"<Government Melon Order: My species = {self.species}, the quantity = {self.quantity}!"
+
+
+    def mark_inspection(self):
+        """Record the fact than an order has been inspected."""
+
+        self.passed_inspection = True
+
+order66 = GovernmentMelonOrder('cantaloupe', 5)
+
+
+
+
